1ffafa994d00ef8369428381ae05ee8a84c650f8
Gokul1456 <gokulavasan145@gmail.com>
2025-12-31 16:37:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div id="root">
       <div>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={instagramLogo} className="logo" alt="Instagram logo" />
         </a>
       </div>
@@ -24,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Instagram logo to visit Instagram
       </p>
-    </>
+    </div>
   )
 }
 
